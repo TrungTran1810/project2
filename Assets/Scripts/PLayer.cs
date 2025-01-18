7e90c21bb75b89deb1bdc50a6e66b8c1b2461871
TrungTran1810 <tranductrung10g@gmail.com>
2025-01-18 21:44:53 +0700

    addEatBbrick

--- Assets/Scripts/PLayer.cs
+++ Assets/Scripts/PLayer.cs
@@ -11,6 +11,10 @@ public class PLayer : MonoBehaviour
     float vertical;
     public float Speed;
     public Joystick joystick;
+    private List<GameObject> Addbrick=new List<GameObject>();
+    public GameObject AddbrickPrefab;
+    public Transform player;
+
     void Update()
     {
         Moving();
@@ -24,16 +28,49 @@ public class PLayer : MonoBehaviour
         vertical = joystick.Vertical;
 
         Vector3 direction = new Vector3(horizontal, 0, vertical); // Horizontal and Vertical map to X and Z
-        transform.Translate(direction * Time.deltaTime*Speed);
+        transform.Translate(direction * Time.deltaTime * Speed);
 
     }
 
-    private void CheckWall()
+    private void EatBrick()
     {
+        Vector3 newPosition;
+        if (Addbrick.Count == 0)
+        {
+            newPosition = player.position - player.up * 03f;
+        }
+        else
+        {
+            newPosition = Addbrick[Addbrick.Count-1].transform.position-player.right*0.3f;
+        }
+
+        GameObject newBrick = Instantiate(AddbrickPrefab, newPosition, Quaternion.identity);
+        Addbrick.Add(newBrick);
+
+        newBrick.transform.SetParent(transform);
+        newBrick.transform.localPosition = new Vector3(0.7f, (+0.2f * Addbrick.Count ), 0);
 
     }
-    private void Eatbrick()
+
+
+
+    private void OnTriggerEnter(Collider other)
     {
+        
+        if (other.gameObject.tag == "Green")
+        {
+           
+            EatBrick();
+            Destroy(other.gameObject);
+            //Invoke("EatBrick", 5f);
+          
 
+        }
+        else if(other.gameObject.tag==null)
+        {
+            return;
+
+        }
     }
+   
 }
